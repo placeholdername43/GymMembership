1f2fb07c115045bf5ee2f149d26173f4e6911a14
Finnadip <42492360+Finnadip@users.noreply.github.com>
2023-11-28 15:47:31 +0000

    Change styling + alter database relationships
    Took memberID out of class
    Added scheduleID to have classes with different schedules
    A classmember table, with gymmemberID & classID

--- gymmanagement/staff.php
+++ gymmanagement/staff.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Staff Management</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous" defer></script>
     <link rel="stylesheet" href="stylingfile.css"> 
 </head>
-<header>    
+    <div class = navbar>
         <nav>
             <ul>
                 <li><a href="homepage.php">Home</a></li>
@@ -14,12 +16,16 @@
                 <li><a href="staff.php">Staff</a></li>
             </ul>
         </nav>
-    </header>
+    </div>
+
+    
 <body>
     <h1>Staff Management</h1>
     
     <!-- ading new staff form -->
     <form action="staff.php" method="post">
+        
+    <div class = "staffform">
         <label for="firstname">First Name:</label>
         <input type="text" id="firstname" name="firstname" required><br>
 
@@ -39,6 +45,7 @@
         <input type="text" id="role" name="role" required><br>
 
         <input type="submit" value="Add Staff">
+</div>
     </form>
 
     <?php
@@ -207,3 +214,5 @@ if (isset($_GET['edit']) && ctype_digit($_GET['edit'])) {
 
 </body>
 </html> 
+
+
